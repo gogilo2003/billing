e7a6463f08a1b693bd5aeeaa5dfdead2627dc12b
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2022-09-11 15:42:42 +0300

    Corrected error in alter_users_table_add_api_token_column migration

--- database/migrations/2022_09_11_153257_alter_users_table_add_api_token_column.php
+++ database/migrations/2022_09_11_153257_alter_users_table_add_api_token_column.php
@@ -13,7 +13,7 @@ class AlterUsersTableAddApiTokenColumn extends Migration
      */
     public function up()
     {
-        if (!Schema::hasColumn(('api_token'))) {
+        if (!Schema::hasColumn('users', 'api_token')) {
             Schema::table('users', function (Blueprint $table) {
                 $table->string('api_token')->nullable()->after('password');
             });
